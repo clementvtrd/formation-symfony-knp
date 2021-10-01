08e818afc27f1d34d93a53e37dada8a9ac7dba14
Clément Vétillard <84911237+clementvtrd@users.noreply.github.com>
2021-10-02 01:50:44 +0200

    test: add Behat definitions

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,8 +2,6 @@
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Tester\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 
 /**
  * Defines application features from the specific context.
@@ -20,76 +18,5 @@ class FeatureContext implements Context
     public function __construct()
     {
     }
-
-    /**
-     * @Given I am an authenticated user
-     */
-    public function iAmAnAuthenticatedUser()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @When I am on the creation page
-     */
-    public function iAmOnTheCreationPage()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @When I fill the name field
-     */
-    public function iFillTheNameField()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @When I fill the description field
-     */
-    public function iFillTheDescriptionField()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @When I fill the price field
-     */
-    public function iFillThePriceField()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @When I press the button :arg1
-     */
-    public function iPressTheButton($arg1)
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Then I should be redirected to the home page
-     */
-    public function iShouldBeRedirectedToTheHomePage()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Then I should see my cake
-     */
-    public function iShouldSeeMyCake()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Then I should see the success message
-     */
-    public function iShouldSeeTheSuccessMessage()
-    {
-        throw new PendingException();
-    }
+    
 }
--- features/cake.feature
+++ features/cake.feature
@@ -3,14 +3,26 @@ Feature: Add a cake
     As an authenticated user
     I should be able to add a cake
 
+    Background:
+        Given these are users:
+        | email | password |
+        | john.doe@knplabs.com | 123456 |
+    
+    Scenario: Signing in
+        Given I am not logged in
+        When I am on "/cakes"
+        And I press "Sign in"
+        Then I should be redirected on "/signin"
+
     Scenario: Add a cake
-     Given I am an authenticated user
-      When I am on the creation page
-      And I fill the name field
-      And I fill the description field
-      And I fill the price field
-      And I press the button "Add cake"
-      Then I should be redirected to the home page
-      And I should see my cake
-      And I should see the success message
+        Given I am logged in as "john.doe@knplabs.com"
+        When I am on "/cakes/create"
+        And I fill "name" with "brownie"
+        And I fill "description" field with "A simple chocolate cake"
+        And I fill "price" field with "40"
+        And I press "Add cake"
+        Then I should be redirected on "/cakes"
+        And I should see "brownie"
+        And I should see "40.00€"
+        And I should see "Bravo !"
      
